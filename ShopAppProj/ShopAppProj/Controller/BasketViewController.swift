d40b3c437e70616a7215129b4d718dd93de65252
KolhoznikFedor <107774521+KirillFasevichDevIos@users.noreply.github.com>
2022-06-22 11:27:01 +0300

    commit finish 5

---
+++ ShopAppProj/ShopAppProj/Controller/BasketViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BasketViewController.swift
+//  ShopAppProj
+//
+//  Created by admin on 22.06.2022.
+//
+
+import UIKit
+
+class BasketViewController: UIViewController {
+
+    @IBOutlet weak var priceLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//
+//        switch segue.identifier {
+//        case "goBasket":
+//            if let selectedIndexPath = collectionView.indexPathsForSelectedItems? .first {
+//                let photo = storeFront[selectedIndexPath.row]
+//                let destinationVC = segue.destination as! StoreFrontDetailViewController
+//                destinationVC.store = photo
+//            }
+//        default:
+//            preconditionFailure("Unexpected segue identifier")
+//        }
+//    }
+}
+    
+
